37ff8eefba874686b12c8e8082a585147fdb1c93
CeeLizzle <88803350+CeeLizzle@users.noreply.github.com>
2023-12-02 22:31:10 +0000

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: '../dist/client',
+    emptyOutDir: false
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 8000, // you can replace this port with any port
+  },
+
+})
+
+
