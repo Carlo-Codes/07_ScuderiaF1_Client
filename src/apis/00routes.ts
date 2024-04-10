92c7f3c71a9b2e66423161c84557e3c8dd6b0ce7
CeeLizzle <88803350+CeeLizzle@users.noreply.github.com>
2024-04-10 23:51:38 +0100

    getting ready for proper .env files

--- src/apis/00routes.ts
+++ src/apis/00routes.ts
@@ -1,7 +1,7 @@
 
 
-const baseUrl = 'http://sucderiaF1LB-1910073435.eu-north-1.elb.amazonaws.com/api/'
-//const baseUrl = 'http://localhost:7000/api/'
+//const baseUrl = 'http://sucderiaF1LB-1910073435.eu-north-1.elb.amazonaws.com/api/'
+const baseUrl = 'http://localhost:7000/api/'
 export const newUserRoute = baseUrl + 'newUser/'
 export const userRoute = baseUrl + 'user/'
 export const leagueRoute = baseUrl + 'league/'
